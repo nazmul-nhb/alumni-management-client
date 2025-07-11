30ca3960508af753e37f6c0c4a700db2575e908a
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-11 12:40:22 +0600

    feat(scripts): added all scripts from nhb-scripts

---
+++ nhb.commit.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+
+import { defineCommitConfig } from 'nhb-scripts';
+
+export default defineCommitConfig({
+	runFormatter: false, // do not run formatter,  use `true` to format before committing
+});
---
+++ nhb.format.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+
+import { defineFormatConfig } from 'nhb-scripts';
+
+export default defineFormatConfig({
+	args: ['--write'], // prettier arguments
+	files: ['.'], // scan everything
+	ignorePath: '.prettierignore', // ignore files/folders mentioned in this file
+});
