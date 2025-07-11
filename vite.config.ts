eed4abeaefbb356066f203ca633e46349fab86d0
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-11 16:42:30 +0600

    feat(tanstack): added tanstack router and query

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		tanstackRouter({
+			target: 'react',
+			autoCodeSplitting: true,
+		}),
+	],
 });
